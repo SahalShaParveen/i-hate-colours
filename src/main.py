6b0e16d3eac3afc7d32e1813ae6f048f101d18a8
SahalShaParveen <sahalshaparveen@gmail.com>
2026-08-17 14:17:15 +0530

    Also displays hex code cuz why not, even tho no one can read it

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 import pyautogui
 import time
 
+def RGBtoHEX(r, g, b): 
+    hex = f"#{r:02x}{g:02x}{b:02x}"
+    return hex
+
 while True: 
     position = pyautogui.position()
     im = pyautogui.screenshot() 
     colour = im.getpixel(position)
-    print(F"{position} | {colour}")
+
+    r, g, b = colour
+    hex = RGBtoHEX(r,g,b)
+
+
+    print(F"{position} | {colour} | {hex}")
     time.sleep(0.05)
+
+
+# Takes a screenshot once every 0.05 seconds tho. 
