9bd682ce9fe6abd1f1d0cee55b796dffad92400c
SahalShaParveen <sahalshaparveen@gmail.com>
2026-08-17 18:52:34 +0530

    Add global hotkey :3

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import pyautogui
+import keyboard
 import time
+
+HOTKEY = "ctrl + shift + z"
 
 def get_cursor_pixel_colour():
     position = pyautogui.position()
@@ -12,12 +15,25 @@ def RGBtoHEX(r, g, b):
     hex = f"#{r:02x}{g:02x}{b:02x}"
     return hex
 
-while True:
-    r, g, b = get_cursor_pixel_colour()
-    hex = RGBtoHEX(r,g,b)
 
-    print(F"({r}, {g}, {b}) | {hex}")
-    time.sleep(0.05)
+state = False
+
+def toggleState(): 
+    global state 
+    state = not state
+
+
+keyboard.add_hotkey(HOTKEY, callback=toggleState)
+
+while True:
+    if state: 
+        r, g, b = get_cursor_pixel_colour()
+        hex = RGBtoHEX(r,g,b)   
+
+        print(F"({r}, {g}, {b}) | {hex}")
+        time.sleep(0.05)
+    else: 
+        time.sleep(0.1)
 
 
 # Takes a screenshot once every 0.05 seconds tho. 
