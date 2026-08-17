f1a1744b20ce6c484a5836535e243dc693148b9a
SahalShaParveen <sahalshaparveen@gmail.com>
2026-08-17 15:40:08 +0530

    A bit of decomposition to make it look prettier ig

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 import pyautogui
 import time
 
+def get_cursor_pixel_colour():
+    position = pyautogui.position()
+    im = pyautogui.screenshot() 
+    colour = im.getpixel(position)
+    return colour
+
+
 def RGBtoHEX(r, g, b): 
     hex = f"#{r:02x}{g:02x}{b:02x}"
     return hex
 
-while True: 
-    position = pyautogui.position()
-    im = pyautogui.screenshot() 
-    colour = im.getpixel(position)
-
-    r, g, b = colour
+while True:
+    r, g, b = get_cursor_pixel_colour()
     hex = RGBtoHEX(r,g,b)
 
-
-    print(F"{position} | {colour} | {hex}")
+    print(F"({r}, {g}, {b}) | {hex}")
     time.sleep(0.05)
 
 
