eca4067253fb5aa710305ed3d6db3ba825d76781
SahalShaParveen <sahalshaparveen@gmail.com>
2026-08-17 13:51:38 +0530

    Prints RGB values of pixel under cursor

--- src/main.py
+++ src/main.py
@@ -2,5 +2,8 @@ import pyautogui
 import time
 
 while True: 
-    print(pyautogui.position()) 
+    position = pyautogui.position()
+    im = pyautogui.screenshot() 
+    colour = im.getpixel(position)
+    print(F"{position} | {colour}")
     time.sleep(0.05)
